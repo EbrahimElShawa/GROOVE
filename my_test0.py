196c785163df59a7f1ec608d57c27eebb11c52f2
EbrahimElShawa <imalwaysforlife@gmail.com>
2024-03-30 09:36:19 +0200

    V0.0.0 Initializing

---
+++ my_test0.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class Main_Window(QMainWindow):
+    def __init__(self):
+        super(Main_Window, self).__init__()     # Calling the superclass constructor with argument 'Main_Window)
+
